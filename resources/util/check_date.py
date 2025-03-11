48894d08fe2461f4800a67ccc6e4add8160da1f9
Kristin Kullberg <kristin.kullberg@gmail.com>
2025-03-11 14:54:45 +0100

    minor spell edit

--- resources/util/check_date.py
+++ resources/util/check_date.py
@@ -17,7 +17,7 @@ def get_date_format_00():
     """
     Determine the date format based on the system's locale.
     Returns the date format string.
-    If format is yyyy-m-dd, two zeroes are added initially to fit date input format. 
+    If format is yyyy-m-dd, two zeros are added initially to fit date input format. 
     """
     loc = locale.getdefaultlocale()[0]
     if loc == 'sv_SE':
